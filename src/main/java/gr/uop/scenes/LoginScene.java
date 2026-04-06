644de7b74166a9b8f805858823e635dc01c84200
Panagiotis Galiotos <panagiwthsgaliwtos@gmail.com>
2026-04-06 18:56:42 +0300

    Update Login Scene

--- src/main/java/gr/uop/scenes/LoginScene.java
+++ src/main/java/gr/uop/scenes/LoginScene.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -19,16 +20,29 @@ public class LoginScene {
         Label title = new Label("Σύνδεση στον Γκρινιάρη");
         title.setStyle("-fx-font-size: 20; -fx-font-weight: bold;");
 
+        Label nameLabel = new Label("Όνομα:");
         TextField nameField = new TextField();
         nameField.setPromptText("Όνομα Παίκτη");
+        nameField.setPrefWidth(220);
+        nameLabel.setMinWidth(110);
+        HBox nameRow = new HBox(10, nameLabel, nameField);
+        nameRow.setAlignment(Pos.CENTER_LEFT);
 
+        Label ipLabel = new Label("Διεύθυνση IP:");
         TextField ipField = new TextField("localhost");
         ipField.setPromptText("Διεύθυνση IP");
+        ipField.setPrefWidth(220);
+        ipLabel.setMinWidth(110);
+        HBox ipRow = new HBox(10, ipLabel, ipField);
+        ipRow.setAlignment(Pos.CENTER_LEFT);
 
-        // Πεδίο για port
         Label portLabel = new Label("Θύρα:");
         TextField portField = new TextField("7777");
         portField.setPromptText("Port");
+        portField.setPrefWidth(220);
+        portLabel.setMinWidth(110);
+        HBox portRow = new HBox(10, portLabel, portField);
+        portRow.setAlignment(Pos.CENTER_LEFT);
 
         Button connectBtn = new Button("Σύνδεση");
         connectBtn.setPrefWidth(200);
@@ -73,9 +87,16 @@ public class LoginScene {
             }
         });
 
-        VBox layout = new VBox(12, title, nameField, ipField, portField, connectBtn, errorLabel);
+        VBox formBox = new VBox(12, title, nameRow, ipRow, portRow, connectBtn, errorLabel);
+        formBox.setAlignment(Pos.CENTER);
+        formBox.setPadding(new Insets(20));
+        formBox.setStyle("-fx-border-color: #cccccc; -fx-border-width: 1;");
+        formBox.setMaxWidth(400);
+
+        VBox layout = new VBox();
         layout.setAlignment(Pos.CENTER);
         layout.setPadding(new Insets(24));
+        layout.getChildren().add(formBox);
 
         return new Scene(layout, 480, 320);
     }
